14ddcdfc89d541c98fcd81e274e282b18bfc2515
glebshapa <63099747+glebshapa@users.noreply.github.com>
2021-01-14 21:39:56 +0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+
+//#pragma GCC optimize("Ofast")
+//#pragma GCC optimize("unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define sz(x) ((ll)(x).size())
+
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+
+template<class T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef long double ld;
+
+const ld PI = 3.1415926535;
+const ll N = 2e5 + 7;
+const ll INF = 1e18 + 7;
+
+void Solve() {
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll tests = 1;
+    //cin >> tests;
+    while(tests--)
+        Solve();
+    return 0;
+}
